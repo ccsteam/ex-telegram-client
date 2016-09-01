1d23d0ac73addf53f35a1c4dc96e5440351d0db9
Andrew Noskov <anoskov@ccsteam.ru>
2016-09-01 15:27:25 +0300

    add PortManager

--- config/dev.exs
+++ config/dev.exs
@@ -5,4 +5,5 @@ config :logger, level: :debug
 config :tg_client,
   daemon: "/usr/local/telegram-cli",
   key: "/usr/local/share/telegram-cli/tg-server.pub",
-  session_env_path: "/tmp/telegram-cli/sessions"
+  session_env_path: "/tmp/telegram-cli/sessions",
+  port_range: 2000..4000
